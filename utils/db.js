e89fdd612cfa0d1dca11238912aaa7eac71f8cc5
Tycee02 <wakilaminat@gmail.com>
2024-09-28 19:32:25 +0100

    db file

---
+++ utils/db.js
@@ -0,0 +1,56 @@
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.connected = false;
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      this.connected = true;
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Failed to connect to MongobDB:', error);
+      this.client = null;
+    }
+  }
+
+  isAlive() {
+    return this.client && this.client.topology && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) {
+      throw new Error('Not connected to the database');
+    }
+    try {
+      return await this.client.db().collection('users').countDocuments();
+    } catch (error) {
+      console.error('Error counting users:', error);
+      throw error;
+    }
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) {
+      throw new Error('Not connected to the database');
+    }
+    try {
+      return await this.client.db().collection('files').countDocuments();
+    } catch (error) {
+      console.error('Error counting files:', error);
+      throw error;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;

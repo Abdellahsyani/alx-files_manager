5f193a1af7760e329baaca2f371e6faacd833347
Abdellahsyani <syaniabdos@gmail.com>
2024-09-28 19:47:23 +0100

    fixed the issue

--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,35 @@
-import {createClient } from 'redis'
-import { promisify } from 'util'
-
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
 class RedisClient {
-	constructor() {
-		this.client = createClient();
-		this.isClientConnected = true;
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
 
-		this.client.on('error', (err) => {
-			console.error("Redis client not connected to the server:", err.message || err.toString());
-			this.isClientConnected = false;
-		});
-		this.client.on('ready', () => {
-			this.isClientConnected = true
-		});
-	}
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-	isAlive() {
-		return this.isClientConnected;
-	}
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
 
-	async get(key) {
-		return promisify(this.client.GET).bind(this.client)(key);
-	}
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
 
-	async set(key, value, timeout) {
-		await promisify(this.client.SETEX)
-		  .bind(this.client)(key, value, timeout);
-	}
-	async del(val) {
-		await promisify(this.client.DEL).bind(this.client)(key);
-	}
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
 
 export const redisClient = new RedisClient();

23dedb4db98bfa9ab3f554b98e964c20f16d04d6
Tycee02 <wakilaminat@gmail.com>
2024-09-29 11:35:42 +0100

    endpoint file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 // Define your routes
 router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats)
-=======
+router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe)
+router.post('/users', UsersController.postNew);
 
->>>>>>> origin/master
+
 module.exports = router;

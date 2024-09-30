d238e25e129c45175529c3369dd23ceecf1e37a3
Tycee02 <wakilaminat@gmail.com>
2024-09-30 14:44:27 +0100

    routes file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
 const AppController = require('../controllers/AppController');
-const FilesController = require('../controllers/FilesController');
+const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
 
 // Define your routes
@@ -9,9 +10,7 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('disconnect', AuthController.getDisconnect);
-router.post('/files', FilesController.postUpload);
-router.get('/users/me', UsersController.getMe)
+router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
-
 
 module.exports = router;

2de3e392696bb4997e07b926588cf637b7d60d55
Tycee02 <wakilaminat@gmail.com>
2024-09-30 17:57:15 +0100

    correction

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,16 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import { redisClient } from '../utils/redis';
+import dbClient from '../utils/db';
 
 export default class AppController {
   static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    console.log('Redis Alive:', redisAlive);
+    console.log('DB Alive:', dbAlive);
+
     res.status(200).send({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      redis: redisAlive,
+      db: dbAlive,
     });
   }
 
@@ -13,6 +18,10 @@ export default class AppController {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
         res.status(200).json({ users: usersCount, files: filesCount });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
       });
   }
 }

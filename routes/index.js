f276a75b137e813fc7eeb8c5ad2b9759f5e04135
Abdellahsyani <syaniabdos@gmail.com>
2024-10-01 23:07:24 +0100

    add empty file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
 
 // Define your routes
 router.get('/status', AppController.getStatus);
@@ -12,5 +13,6 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
+router.post('/files'. FilesController.postUpload);
 
 module.exports = router;
